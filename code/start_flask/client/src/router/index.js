c0ab29642e9b3d83c3b348501ac88b158b360bd5
Daria Shtanakova <dshtanakova@gmail.com>
2020-04-23 00:19:37 +0300

    add axios to connect client vue to server flask

--- code/start_flask/client/src/router/index.js
+++ code/start_flask/client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Ping from './components/Ping.vue';
+import Ping from '../components/Ping.vue';
 
 Vue.use(Router);
 
@@ -12,6 +12,6 @@ export default new Router({
       path: '/ping',
       name: 'Ping',
       component: Ping,
-    }
+    },
   ],
 });
--- code/start_flask/start_app.py
+++ code/start_flask/start_app.py
@@ -1,21 +1,23 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 
+# configuration
+DEBUG = True
+
+# instantiate the app
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
-@app.route('/test_func')
-def get_ses():
- 	return render_template('index.html')
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
